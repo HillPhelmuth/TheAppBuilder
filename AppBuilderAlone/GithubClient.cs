2f53e6a3d20837e5c6d28c8998adbfdca6f06c75
HillPhelmuth <46843652+HillPhelmuth@users.noreply.github.com>
2020-12-26 18:40:15 -0600

    Code cleanup and accesibilty/seo quick fixes

--- AppBuilderAlone/GithubClient.cs
+++ AppBuilderAlone/GithubClient.cs
@@ -14,21 +14,16 @@ namespace AppBuilder.Client
         private readonly HttpClient _client;
         private readonly string baseUrl = @"https://api.github.com/repos";
 
-        public GithubClient(HttpClient client, IConfiguration config)
+        public GithubClient(HttpClient client)
         {
             _client = client;
             _client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3.raw");
-            //_client.DefaultRequestHeaders.Add("Authorization",config.GetValue<string>("GithubToken"));
-            //client.DefaultRequestHeaders.Add("Access-Control-Allow-Origin", "https://render.githubusercontent.com");
         }
 
 
         public async Task<Stream> CodeFromPublicRepo(string githubName, string repoName, string filepath)
         {
-            //if (!filepath.Contains("."))
-            //{
-            //    return "Nope!, provide a file extension. I suggest '.cs'";
-            //}
+            
             var sw = new Stopwatch();
             sw.Start();
             var code = await _client.GetStreamAsync($"{baseUrl}/{githubName}/{repoName}/contents/{filepath}");
@@ -37,23 +32,5 @@ namespace AppBuilder.Client
             return code;
         }
 
-        //public async Task<Stream> DownloadGithubZip(string org, string repoName)
-        //{
-        //    var sw = new Stopwatch();
-        //    sw.Start();
-        //    if (string.IsNullOrWhiteSpace(org) || string.IsNullOrWhiteSpace(repoName)) return null;
-
-
-        //   // return await _client.GetStreamAsync($"http://localhost:7071/api/DownloadGithubZip/{org}/{repoName}");
-
-        //    //var resultString = await _client.GetStringAsync($"{baseUrl}/{org}/{repoName}");
-        //    sw.Stop();
-        //    var repoDetails = await _client.GetFromJsonAsync<RepoDetails>($"{baseUrl}/{org}/{repoName}");
-
-        //    Console.WriteLine($"Download \r\n {repoDetails}\r\n from Github in {sw.ElapsedMilliseconds}");
-
-        //    var result = await _client.GetStreamAsync($"{baseUrl}/{org}/{repoName}/zipball/master");
-        //    return result;
-        //}
     }
 }
