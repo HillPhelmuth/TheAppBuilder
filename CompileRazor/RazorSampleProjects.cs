161e25424b7b53560e1a3fe36ea65ccc9f00c87e
HillPhelmuth <46843652+HillPhelmuth@users.noreply.github.com>
2020-12-27 18:35:54 -0600

    Update razor sample

--- CompileRazor/RazorSampleProjects.cs
+++ CompileRazor/RazorSampleProjects.cs
@@ -11,10 +11,15 @@ namespace AppBuilder.CompileRazor
             {
                 Name = RazorConstants.DefaultComponentName,
                 FileType = FileType.Razor,
-                Content = @"<h1>Hello World</h1>
-<h3>@testString</h3>
-<button @onclick=""GetTestString"">Test</button>
-<Child ChangeParent=""HandleChildString""/>
+                Content = @"<div class=""text-center"">
+    <h1>Hello Party People!</h1>
+    <h2>This is your App Builder sample app</h2>
+    <h3>@testString</h3>
+    <button class=""btn btn-primary"" @onclick=""GetTestString"">Test</button>
+    <Child ChangeParent=""HandleChildString"" />
+</div>
+<hr />
+<TestData />
 @code
 {
     string testString = ""I Am Here!"";
@@ -24,7 +29,7 @@ namespace AppBuilder.CompileRazor
     }
     void HandleChildString(string str)
     {
-        testString = str;
+       testString = str;
     }
 }"
             },
@@ -32,7 +37,7 @@ namespace AppBuilder.CompileRazor
             {
                 Name = "Child.razor",
                 FileType = FileType.Razor,
-                Content = @"<button @onclick=""SayHello"">Child Button</button>
+                Content = @"<button class=""btn btn-primary"" @onclick=""SayHello"">Child Button</button>
 
 @code {
     [Parameter]
@@ -50,10 +55,86 @@ namespace AppBuilder.CompileRazor
                 FileType = FileType.Class,
                 Content = @"public class Test
 {
+    public Test(string title, string topic, int difficulty)
+    {
+        Title = title;
+        Topic = topic;
+        Difficulty = difficulty;
+    }
+    public string Title {get;set;}
+    public string Topic {get;set;}
+    public int Difficulty {get;set;}
 	public static string GetString()
     {
         return ""Hello From Test Class"";
     }
+}"
+            },
+            new ProjectFile
+            {
+                Name = "TestData.razor",
+                FileType = FileType.Razor,
+                Content = @"<h3 class=""text-center"">TestData.razor</h3>
+<div class=""text-center"">
+    <p style=""font-size:.8rem"">click header to sort</p>
+<table style=""margin:auto"">
+    <tr>
+        <th @onclick=""@(() => ChangeOrder(""Title""))"">Title</th>
+        <th @onclick=""@(() => ChangeOrder(""Topic""))"">Topic</th>
+        <th @onclick=""@(() => ChangeOrder(""Difficulty""))"">Difficulty</th>
+    </tr>
+    @foreach (var test in tests)
+    {
+        <tr>
+            <td>@test.Title</td>
+            <td>@test.Topic</td>
+            <td>@test.Difficulty</td>
+        </tr>
+    }
+</table>
+</div>
+
+@code{
+
+    List<Test> tests = new List<Test>
+        {
+            new Test(""Algebra"", ""Math"",5),
+            new Test(""Dickins"", ""Literature"", 4),
+            new Test(""Physics"", ""Science"", 9),
+            new Test(""US States"", ""Geography"", 2),
+            new Test(""CSharp"", ""Programming"", 1),
+            new Test(""Algebra2"", ""Math"",7),
+            new Test(""Faulkner"", ""Literature"", 6),
+            new Test(""Physics2"", ""Science"", 10),
+            new Test(""Country Capitols"", ""Geography"", 5),
+            new Test(""C# Razor"", ""Programming"", 1)
+        };
+    private bool isDsc;
+    private void ChangeOrder(string orderByCol)
+    {
+        if (isDsc)
+        {
+            tests = orderByCol switch
+            {
+                ""Title"" => tests.OrderBy(x => x.Title).ToList(),
+                ""Topic"" => tests.OrderBy(x => x.Topic).ToList(),
+                _ => tests.OrderBy(x => x.Difficulty).ToList()
+            };
+        }
+        else
+        {
+            tests = orderByCol switch
+            {
+                ""Title"" => tests.OrderByDescending(x => x.Title).ToList(),
+                ""Topic"" => tests.OrderByDescending(x => x.Topic).ToList(),
+                _ => tests.OrderByDescending(x => x.Difficulty).ToList()
+            };
+        }
+
+        isDsc = !isDsc;
+        StateHasChanged();
+    }
+    
 }"
             }
         };
